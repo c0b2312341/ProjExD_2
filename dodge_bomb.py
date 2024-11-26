50c6b571c2a69c7d8d0817fc144eaf2310af3d28
S.Fujimoto <c0b2312341@edu.teu.ac.jp>
2024-11-26 16:43:32 +0900

    演習1: GAME OVER画面

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -26,6 +26,41 @@ def check_bound(rct):
         tate = False
     return yoko, tate
 
+def game_over(img):
+    """
+    黒い背景と5秒間のカウントを行う
+    引数: 背景用のimg
+    (bg_imgの想定)
+    """
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    clock = pg.time.Clock()
+
+    black_img = pg.Surface((1100, 650))
+    pg.draw.rect(black_img, (0, 0, 0), pg.Rect(0, 0, 1100, 650))  # 黒色の背景を設定
+    black_img.set_alpha(90)  # 透明度90
+    kk_right_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9)
+    kk_left_img = pg.transform.rotozoom(pg.image.load("fig/8.png"), 0, 0.9)
+
+    fonto = pg.font.Font(None, 80)  # fontの設定
+    text = fonto.render("Game Over", True, (255, 255, 255))  # GAME OVERと文字色の設定
+    text_rect = text.get_rect()
+    text_rect.center = 550, 325
+    kk_right_rect = kk_right_img.get_rect()
+    kk_right_rect.center = 550 + 200, 325
+    kk_left_rect = kk_left_img.get_rect()
+    kk_left_rect.center = 550 - 200, 325
+
+    # 1秒の処理を5回繰り返す, 5秒間
+    for i in range(5):
+        screen.blit(img, [0, 0])
+        screen.blit(black_img, [0, 0])
+        screen.blit(text, text_rect)
+        screen.blit(kk_right_img, kk_right_rect)
+        screen.blit(kk_left_img, kk_left_rect)
+        pg.display.update()
+        clock.tick(1)
+        print(f"{i + 1}秒")
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((WIDTH, HEIGHT))
@@ -47,6 +82,7 @@ def main():
             if event.type == pg.QUIT: 
                 return
             if kk_rct.colliderect(bb_rct):
+                game_over(bg_img)
                 print("GAME OVER")
                 return  # gameover
         screen.blit(bg_img, [0, 0]) 
@@ -55,8 +91,8 @@ def main():
         sum_mv = [0, 0]
         for key, tpl in DELTA.items():
             if key_lst[key]:
-                sum_mv[0] = tpl[0]
-                sum_mv[1] = tpl[1]
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
         kk_rct.move_ip(sum_mv)
         #こうかとんが画面内なら元の場所に戻す
         if check_bound(kk_rct) != (True, True):
@@ -67,6 +103,7 @@ def main():
             vx *= -1
         if tate == False:
             vy *= -1
+
         screen.blit(kk_img, kk_rct)
         screen.blit(bb_img, bb_rct)
         pg.display.update()
